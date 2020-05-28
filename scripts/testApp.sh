44ccdbf3b62ae2af0b0c0491fb7bb0d465c275fd
Paulo Daniel Ambrosio <pdajgs@localhost.localdomain>
2020-05-28 18:04:25 -0300

    scripts v1

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -sv localhost:80/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"test@test","comment":"I am alive!!!","content_id":99}'
 
 curl -sv localhost:8000/api/comment/list/99
