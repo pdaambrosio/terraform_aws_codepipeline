4233fc3615aa7fd211cc7ff50341776d8bc81078
Paulo Daniel Ambrosio <pdajgs@localhost.localdomain>
2020-05-30 17:30:23 -0300

    wait

--- scripts/beforeBuild.sh
+++ scripts/beforeBuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # validação minima deploy
-docker=i$(systemctl status docker)
+time=$(sleep 60)
+docker=$(systemctl status docker)
 codedeploy=$(systemctl status codedeploy-agent)
 compose=$(docker-compose version)
